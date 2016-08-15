6950ebb75ed01d73becb2b292bf2761629d94c0f
USER <user@USER-no-MacBook-Air.local>
2016-08-16 00:58:36 +0200

    Initial commit, application working now but no styling what so ever

---
+++ app/controllers/base_controller.rb
@@ -0,0 +1,30 @@
+class BaseController < ApplicationController
+  def generate_plant
+    contents = File.read('config/data/en.json')
+    @plants = JSON.parse(contents)
+    @plant = @plants.sample
+    @url = url_for(controller: 'base',
+            action: 'verify',
+            plant: @plant['name'],
+            only_path: true)
+  end
+
+  def index
+    generate_plant
+  end
+
+  def verify
+    generate_plant
+
+    name = params[:plant].downcase
+    @verify_plant = @plants.find { |plant| plant['name'].to_s.downcase == name }
+    raise "what's a #{name}" if @verify_plant.nil?
+
+    is = params[:is].downcase
+    if is != 'fruit' && is != 'vegetable'
+      raise "scuz me, wat is a '#{is}'"
+    end
+
+    @correct = is == @verify_plant['type'].downcase
+  end
+end
